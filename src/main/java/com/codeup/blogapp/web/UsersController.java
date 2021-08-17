bbdfc160ac57022231a3604e4774222dd0da77cc
ricardofigueroa <rfigueroa2317@gmail.com>
2021-08-16 21:13:00 -0500

    update

--- src/main/java/com/codeup/blogapp/web/UsersController.java
+++ src/main/java/com/codeup/blogapp/web/UsersController.java
@@ -1,7 +1,16 @@
 package com.codeup.blogapp.web;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 public class UsersController {
 
+    @RestController
+@RequestMapping(value = "/api/users", headers = "Accept=application/json")
+    public class PostsController {
 
+
+    }
 
 }
